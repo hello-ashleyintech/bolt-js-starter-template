9a69bc9c46be181ff13db0edb03b0325e31c9c89
Sarah Jiang <sarahjiang@slack-corp.com>
2022-06-10 11:55:54 -0700

    Add example manifest ts and js, use mod.js bundled from deno-slack-sdk

---
+++ manifest.js
@@ -0,0 +1,41 @@
+const { DefineFunction, Manifest, Schema } = require('./mod');
+
+const ReverseFunction = DefineFunction({
+  callback_id: 'reverse',
+  title: 'Reverse',
+  description: 'JSTakes a string and reverses it',
+  source_file: 'JSfunctions/reverse.ts',
+  input_parameters: {
+    properties: {
+      stringToReverse: {
+        type: Schema.types.string,
+        description: 'JSThe string to reverse',
+      },
+      channel_id: {
+        type: 'JSslack#/types/channel_id',
+        description: 'channel ID',
+      },
+    },
+    required: ['stringToReverse'],
+  },
+  output_parameters: {
+    properties: {
+      reverseString: {
+        type: Schema.types.string,
+        description: 'The string in reverse',
+      },
+    },
+    required: ['reverseString'],
+  },
+});
+
+const manifest = Manifest({
+  name: 'reverse',
+  description: 'JSReverse a string',
+  icon: 'JSassets/icon.png',
+  functions: [ReverseFunction],
+  outgoingDomains: [],
+  botScopes: ['JScommands', 'JSchat:write', 'JSchat:write.public'],
+});
+
+module.exports = manifest;
